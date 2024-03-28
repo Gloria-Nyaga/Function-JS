a6cd1be1a7ea141a64c53ef7da4164a44b25d271
Gloria-Nyaga <nyagagakiigloria@gmail.com>
2024-03-28 19:35:48 +0300

    added file

---
+++ functions.js
@@ -0,0 +1,69 @@
+//Question 1
+// Given an array of strings, use a function to reverse all the elements in the string in ascending order and the specific elements in descending order
+
+function countries (){
+    let arr1 = ["Burundi", "Malawi", "Zambia", "Tanzania", "Uganda"];
+    console.log(arr1.sort());
+    console.log(arr1.reverse());
+}
+countries()
+
+
+//Question 2
+//Using JS functions and an array of numbers, return positive if an element within the array is positive, negative if an element is negative, else zero
+function numbers() {
+    let arr =arr.forEach(element => {
+       if (element > 0) {
+         console.log("positive");
+
+       } else if (element < 0) {
+         console.log("negative");
+       } 
+       
+       else {
+         console.log("zero");
+        }
+
+    });
+
+   }
+
+
+   //Question 3
+   //Given an array of objects, where each object represents an employee with an id, name, and salary property, write a function that returns a new array of employee objects sorted by their salary in ascending order.
+let employeDetails = [
+    { id: 8 },
+    { name: "Gloria" },
+    { salary: [50000, 30000, 80000, 5000] },
+  ];
+  employeDetails.sort(function(x, y) {
+    return x.salary - (y.salary);
+  });
+  employeDetails.forEach((person) => console.log(employeDetails.salary));
+ console.log(employeDetails);
+
+
+
+ //Question 4
+ //Write a function that accepts an array of numbers and uses the forEach() method to console.log each number multiplied by 2.
+function multiply(numbers) {
+    numbers.forEach((number) => {
+       console.log(number * 2);
+    });
+   }
+   multiply([1, 2, 3, 4]);
+
+
+   //Question 5
+   //Write a function that takes in an array of numbers and consoles the first four items multiplied by 8 and the last two added by 5. Console the array with the new values
+   
+   function changeArray2(arr) {
+     const firstFour = arr.slice(0, 4).map(item => item * 8);
+
+     const lastTwo = arr.slice(-2).map(item => item + 5);
+
+     const newArr = firstFour.concat(lastTwo);
+     
+     console.log(newArr);
+   }
+   changeArray2([10, 20, 30, 40, 50, 60]);
